b92e29779aba845f4980923cea8877cc2795b1cc
Daniel Weir <97197253+8pplexd@users.noreply.github.com>
2022-01-07 01:25:32 +0000

    searchquery

--- main.py
+++ main.py
@@ -7,17 +7,10 @@ app = Flask(__name__, static_url_path='')
 def index():
   return render_template('index.html')
 
-@app.route('/tiger')
-def tiger():
-  return render_template('myth.html')
-
 @app.route('/vidios/<path:path>')
 def vidios(path):
   return send_from_directory('vidios',path)
 
-@app.route('/donut')
-def donut():
-  return "donutf"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
